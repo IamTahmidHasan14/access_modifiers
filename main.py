fe3a58b14e17e6a9e835e6d8030f020e79ff6333
Tahmid Hasan <67729687+IamTahmidHasan14@users.noreply.github.com>
2023-09-08 19:02:41 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+class Students2:
+
+  def __init__(self):
+    self.__name = "Tahmid"
+
+
+s2 = Students2()
+# print (s2.__name)   #it can't run directly
+print(s2._Students2__name)  #it can run indirectly
